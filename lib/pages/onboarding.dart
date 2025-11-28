26365b6b21fc1b5eef09ecb72efaa3f1d393b7d7
Kevin Leonardi <kevinleonardi@yahoo.com>
2025-11-28 14:45:28 +0700

    create onboarding page

--- lib/pages/onboarding.dart
+++ lib/pages/onboarding.dart
@@ -16,9 +16,70 @@ class _OnboardingState extends State<Onboarding> {
           children: [
             Image.asset(
               "images/onboard.jpg",
-              height: MediaQuery.of(context).size.height / 2,
+              height: MediaQuery.of(context).size.height / 1.6,
               width: MediaQuery.of(context).size.width,
               fit: BoxFit.cover,
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height / 1.9,
+              ),
+              width: MediaQuery.of(context).size.width,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: 20),
+                  Text(
+                    "Get The Latest and Up to date\nNews Easily With Us",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Image.asset(
+                    "images/newspaper.png",
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    margin: EdgeInsets.only(left: 40, right: 40),
+                    child: Material(
+                      elevation: 3.0,
+                      borderRadius: BorderRadius.circular(30),
+                      child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          color: Color(0xff3280ef),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Center(
+                          child: Text(
+                            "Get Started",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 25,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
